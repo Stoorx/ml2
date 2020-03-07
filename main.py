1cf771e6f19b0e52146df7aee6f3674dba9f0ec7
Stoorx <antiproton2013@yandex.ru>
2020-03-07 16:14:15 +0300

    Add ridge regression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.linear_model import Ridge
+
 
 class Entry:
     def __init__(self, x: np.ndarray, y: float):
@@ -19,8 +21,31 @@ if __name__ == '__main__':
         for i in range(test_size):
             line = list(map(lambda x: float(x), file.readline().split(' ')))
             testSet.append(Entry(np.array(line[:-1]), line[-1]))
+    # Ridge
+    x = np.array(
+        list(map(lambda o: o.x, trainSet))
+    )
+    y = np.array(
+        list(map(lambda o: o.y, trainSet))
+    )
+    ridge = Ridge(solver='svd')
+    ridge.fit(x, y)
 
+    test_x = np.array(
+        list(map(lambda o: o.x, testSet))
+    )
+    test_y = np.array(
+        list(map(lambda o: o.y, testSet))
+    )
 
+    ridge_predicted = ridge.predict(test_x)
 
+    ridge_smape = 0.0
+    for i in range(len(ridge_predicted)):
+        ridge_smape += abs(ridge_predicted[i] - test_y[i]) / ((abs(ridge_predicted[i]) + abs(test_y[i])) / 2)
+
+    ridge_smape /= len(ridge_predicted)
+
+    print(f"Ridge regression error: {ridge_smape :.3f}")
 
 
